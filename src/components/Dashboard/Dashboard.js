9a933d60953d315b989034141dd3053754ece2f7
masharzy <horsnbd@gmail.com>
2022-04-04 08:57:53 +0600

    Dashboard Done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Dashboard.css";
 import {
   Area,
   AreaChart,
@@ -56,93 +57,104 @@ const Dashboard = () => {
     },
   ];
   return (
-    <div className="container">
-      <div className="row mt-5 text-center" style={{ height: "100vh" }}>
-        <div className="col-md-6">
-          <h1>Month Wise Cell</h1>
-          <ResponsiveContainer width="100%" height={500}>
-            <LineChart width={500} data={data}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="month" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="sell"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
+    <div className="dashboard">
+      <div className="banner text-center text-white">
+        <h1>Dashboard</h1>
+      </div>
+      <div className="container">
+        <div className="row text-center" style={{ height: "100vh" }}>
+          <div className="col-md-6">
+            <div className="chart-head">
+              <h1>Month Wise Cell</h1>
+            </div>
+            <ResponsiveContainer width="100%" height={500}>
+              <LineChart width={500} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="sell"
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
 
-        <div className="col-md-6">
-          <h1>Investment Vs Revenue</h1>
-          <ResponsiveContainer width="100%" height={500}>
-            <AreaChart width={500} data={data}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="month" />
-              <YAxis />
-              <Tooltip />
-              <Area
-                dataKey="investment"
-                stroke="#8884d8"
-                fill="#8884d8"
-                type="monotone"
-              />
-              <Area
-                dataKey="revenue"
-                stroke="#82ca9d"
-                fill="#82ca9d"
-                type="monotone"
-              />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-        {/* </div>
+          <div className="col-md-6">
+            <div className="chart-head">
+              <h1>Investment Vs Revenue</h1>
+            </div>
+            <ResponsiveContainer width="100%" height={500}>
+              <AreaChart width={500} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                <YAxis />
+                <Tooltip />
+                <Area
+                  dataKey="investment"
+                  stroke="#8884d8"
+                  fill="#8884d8"
+                  type="monotone"
+                />
+                <Area
+                  dataKey="revenue"
+                  stroke="#82ca9d"
+                  fill="#82ca9d"
+                  type="monotone"
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          </div>
 
-      <div className="row"> */}
-        <div className="col-md-6">
-          <h1>Investment Vs Revenue</h1>
-          <ResponsiveContainer width="100%" height={500}>
-            <BarChart width={500} data={data}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="month" />
-              <YAxis dataKey="investment" />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="investment" stackId="a" fill="#8884d8" />
-              <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
+          <div className="col-md-6">
+            <div className="chart-head">
+              <h1>Investment Vs Revenue</h1>
+            </div>
+            <ResponsiveContainer width="100%" height={500}>
+              <BarChart width={500} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                <YAxis dataKey="investment" />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+                <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
 
-        <div className="col-md-6">
-          <h1>Investment Vs Revenue</h1>
-          <ResponsiveContainer width="100%" height={500}>
-            <PieChart width={400}>
-              <Tooltip />
-              <Pie
-                data={data}
-                dataKey="investment"
-                cx="50%"
-                cy="50%"
-                outerRadius={60}
-                fill="#8884d8"
-              />
-              <Pie
-                data={data}
-                dataKey="revenue"
-                cx="50%"
-                cy="50%"
-                innerRadius={70}
-                outerRadius={90}
-                fill="#82ca9d"
-                label
-              />
-            </PieChart>
-          </ResponsiveContainer>
+          <div className="col-md-6">
+            <div className="chart-head">
+              <h1>Investment Vs Revenue</h1>
+            </div>
+            <ResponsiveContainer width="100%" height={500}>
+              <PieChart width={400}>
+                <Tooltip />
+                <Pie
+                  data={data}
+                  dataKey="investment"
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={60}
+                  fill="#8884d8"
+                />
+                <Pie
+                  data={data}
+                  dataKey="revenue"
+                  cx="50%"
+                  cy="50%"
+                  innerRadius={70}
+                  outerRadius={90}
+                  fill="#82ca9d"
+                  label
+                />
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
         </div>
       </div>
     </div>
